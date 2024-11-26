13d39cc9cbf7888dd6ecbee91b3892838fa503fc
azureossdemo <ossdemo454242@outlook.com>
2024-11-26 10:49:46 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install the local .deb package
-dpkg -i /home/site/libglib2.0-0_2.74.6-2+deb12u4_amd64.deb
+dpkg -i /home/site/wwwroot/libglib2.0-0_2.74.6-2+deb12u4_amd64.deb
 
 # If there are any missing dependencies, fix them
 apt-get install -f -y
